459eac63fb038a0d0fe79d358cb2b33cadaee990
Chris W <chriswilliford200@yahoo.com>
2023-12-18 12:10:28 -0800

    Added an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const apiRouter = require("./api");
 
-router.get("/hello/world", function (req, res) {
-	res.cookie("XSRF-TOKEN", req.csrfToken());
-	res.send("Hello World!");
-});
+// router.get("/hello/world", function (req, res) {
+// 	res.cookie("XSRF-TOKEN", req.csrfToken());
+// 	res.send("Hello World!");
+// });
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
@@ -14,5 +15,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+router.use("/api", apiRouter);
 
 module.exports = router;
