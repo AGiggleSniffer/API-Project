f55e17bb4ca7585a76296f0811fe53a085a01a15
Chris W <chriswilliford200@yahoo.com>
2024-01-05 14:12:17 -0800

    post and error handler + some tweaks to user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,18 +1,56 @@
 const router = require("express").Router();
+const { spots } = require("../../db/models");
 
 // Add a spot with validation
-router.post("/", async (req, res) => {
+router.post("/", async (req, res, next) => {
 	const { user } = req;
-	if (user) {
-		const safeUser = {
-			id: user.id,
-			email: user.email,
-			username: user.username,
-		};
-		return res.json({
-			user: safeUser,
+
+	if (!user) return res.json({ user: null });
+
+	const { address, city, state, country, lat, lng, name, description, price } =
+		req.body;
+
+	try {
+		const newSpot = await spots.create({
+			user_id: user.id,
+			address: address,
+			city: city,
+			state: state,
+			country: country,
+			lat: lat,
+			lng: lng,
+			name: name,
+			description: description,
+			price: price,
 		});
-	} else return res.json({ user: null });
+
+		res.status(201);
+
+		return res.json({
+			id: user.id,
+			...newSpot,
+		});
+	} catch (e) {
+		res.status(400);
+		e.message = "Bad Request";
+		return next(e);
+	}
+});
+
+router.delete("/:id", async (req, res) => {});
+
+// spot generic error handler
+router.use((err, req, res, next) => {
+    const errors = {};
+    err.errors.forEach(element => {
+		const { path, message } = element;
+		errors[path] = message;
+	});
+
+	return res.json({
+		message: err.message,
+        errors: errors,
+	});
 });
 
 module.exports = router;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -32,8 +32,8 @@ router.post("/", validateSignup, async (req, res) => {
 	const user = await User.create({ firstName, lastName, email, username, hashedPassword });
 
   const safeUser = {
-    firstName: user.firstName,
-    lastName: user.lastName,
+    	firstName: user.firstName,
+    	lastName: user.lastName,
 		id: user.id,
 		email: user.email,
 		username: user.username,
