58293b6de8e680b82f7afc2248e3f2ef7750b189
Chris W <chriswilliford200@yahoo.com>
2024-01-04 18:12:53 -0800

    created tables

---
+++ backend/db/models/spot_images.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class spot_images extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  spot_images.init({
+    spot_id: DataTypes.INTEGER,
+    url: DataTypes.STRING,
+    preview: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'spot_images',
+  });
+  return spot_images;
+};
